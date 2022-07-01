fb4f5bfc605da3245672ecf4834ffffe7bc7d97b
maivn <91010226+maivn@users.noreply.github.com>
2022-07-01 10:13:47 +0700

    Add files via upload

--- deploy.py
+++ deploy.py
@@ -1,18 +1,16 @@
-# Modified by Augmented Startups 2021
-# Face Landmark User Interface with StreamLit
-# Watch Computer Vision Tutorials at www.augmentedstartups.info/YouTube
 import streamlit as st
-import mediapipe as mp
 import cv2
+import mediapipe as mp
 import numpy as np
-import tempfile
+import threading
+import tensorflow as tf
+
 import time
 from PIL import Image
 
 mp_drawing = mp.solutions.drawing_utils
 mp_face_mesh = mp.solutions.face_mesh
 
-DEMO_VIDEO = 'demo.mp4'
 
 
 st.title('Vo Nhu Mai graduation thesis')
@@ -37,41 +35,101 @@ st.sidebar.subheader('App modes')
 
 
 @st.cache()
-def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    # initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = image.shape[:2]
 
-    # if both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return image
 
-    # check to see if the width is None
-    if width is None:
-        # calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
 
-    # otherwise, the height is None
-    else:
-        # calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
 
-    # resize the image
-    resized = cv2.resize(image, dim, interpolation=inter)
+    mpPose = mp.solutions.pose
+    pose = mpPose.Pose()
+    mpDraw = mp.solutions.drawing_utils
 
-    # return the resized image
-    return resized
+    cap = cv2.VideoCapture(0)
 
+    model = tf.keras.models.load_model("model.h5")
+
+
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps_input = int(cap.get(cv2.CAP_PROP_FPS))
+
+    n_time_steps = 40
+    lm_list = []
+    Action = ["STOP",
+              "THIS MARSHALLER",
+              "PROCEED TO NEXT MARSHALLER ON THE RIGHT",
+              "PROCEED TO NEXT MARSHALLER ON THE LEFT",
+              "PERSONNEL APPROACH AIRCRAFT ON THE RIGHT",
+              "PERSONNEL APPROACH AIRCRAFT ON THE LEFT",
+              "NORMAL",
+              "TURN TO THE LEFT",
+              "TURN TO THE RIGHT",
+              "SLOW DOWN",
+              "MOVE FORWARD"
+              ]
+
+
+    def make_landmark_timestep(results):
+        c_lm = []
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            c_lm.append(lm.x)
+            c_lm.append(lm.y)
+            c_lm.append(lm.z)
+            c_lm.append(lm.visibility)
+        return c_lm
+
+
+    def draw_landmark_on_image(mpDraw, results, img):
+        mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            # print(id, lm)
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+        return img
+
+
+    def draw_class_on_image(label, img):
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        bottomLeftCornerOfText = (10, 30)
+        fontScale = 1
+        fontColor = (0, 255, 0)
+        thickness = 2
+        lineType = 2
+        cv2.putText(img, label,
+                    bottomLeftCornerOfText,
+                    font,
+                    fontScale,
+                    fontColor,
+                    thickness,
+                    lineType)
+        return img
+
+
+    # Đưa vào nhận diện
+    def detectpose(model, lm_list):
+        global label
+        lm_list = np.array(lm_list)
+        lm_list = np.expand_dims(lm_list, axis=0)
+        # print(lm_list.shape)
+        results = model.predict(lm_list)
+
+        print(results)
+        xacsuat = results.tolist()
+        print(xacsuat)
+        xacsuat = xacsuat[0]
+        hanhdong = xacsuat.index(max(xacsuat))
+        print("Vitri ")
+        print(hanhdong)
+        label = Action[hanhdong]
+        print(label)
+        return label
+
+
+    i = 0
 
 app_mode = st.sidebar.selectbox('Please Select',
-['About My Project', 'Detect signal', 'Signal check']
-)
+                                ['About My Project', 'Detect signal', 'Signal check']
+                                )
 
 if app_mode == 'About My Project':
     st.markdown(
@@ -97,7 +155,7 @@ if app_mode == 'About My Project':
             I am ** Vo Nhu Mai ** from class **17ĐHKT01**. \n
 
             This is my graduation thesis \n
-           
+
            This application will check the movement of aircraft ramp marshaller. There are 2 modes: Detect Signals and Check Signal. \n
 
             So choose the mode you want and have fun!!
@@ -106,7 +164,6 @@ if app_mode == 'About My Project':
 elif app_mode == 'Detect signal':
 
     st.set_option('deprecation.showfileUploaderEncoding', False)
-
 
     st.sidebar.markdown('---')
     st.markdown(
@@ -130,26 +187,7 @@ elif app_mode == 'Detect signal':
 
     stframe = st.empty()
 
-    cap = cv2.VideoCapture(0)
-
-
-
-
-    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps_input = int(cap.get(cv2.CAP_PROP_FPS))
-
-    # codec = cv2.VideoWriter_fourcc(*FLAGS.output_format)
-    codec = cv2.VideoWriter_fourcc('V', 'P', '0', '9')
-    out = cv2.VideoWriter('output1.mp4', codec, fps_input, (width, height))
-
-    st.sidebar.text('Input Video')
-    st.sidebar.video(tfflie.name)
-    fps = 0
-    i = 0
-    drawing_spec = mp_drawing.DrawingSpec(thickness=2, circle_radius=2)
-
-    kpi1, kpi2= st.beta_columns(3)
+    kpi1, kpi2= st.beta_columns(2)
 
     with kpi1:
         st.markdown("**FrameRate**")
@@ -162,57 +200,33 @@ elif app_mode == 'Detect signal':
 
     st.markdown("<hr/>", unsafe_allow_html=True)
 
-    with mp_face_mesh.FaceMesh(
-            min_detection_confidence=detection_confidence,
-            min_tracking_confidence=tracking_confidence,
-            max_num_faces=max_faces) as face_mesh:
-        prevTime = 0
+    with    success, img = cap.read()
+            imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            results = pose.process(imgRGB)
+            i = i + 1
 
-        while vid.isOpened():
-            i += 1
-            ret, frame = vid.read()
-            if not ret:
-                continue
+            if results.pose_landmarks:
+                c_lm = make_landmark_timestep(results)
+                lm_list.append(c_lm)
+                if len(lm_list) == n_time_steps:
+                    # Nhận diện
+                    t1 = threading.Thread(target=detectpose, args=(model, lm_list,))
+                    t1.start()
+                    lm_list = []
 
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = face_mesh.process(frame)
+                img = draw_landmark_on_image(mpDraw, results, img)
 
-            frame.flags.writeable = True
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            img = draw_class_on_image(label, img)
+            cv2.imshow("Image", img)
+            if cv2.waitKey(1) == ord('q'):
+            break
 
-            face_count = 0
-            if results.multi_face_landmarks:
-                for face_landmarks in results.multi_face_landmarks:
-                    face_count += 1
-                    mp_drawing.draw_landmarks(
-                        image=frame,
-                        landmark_list=face_landmarks,
-                        connections=mp_face_mesh.FACE_CONNECTIONS,
-                        landmark_drawing_spec=drawing_spec,
-                        connection_drawing_spec=drawing_spec)
-            currTime = time.time()
-            fps = 1 / (currTime - prevTime)
-            prevTime = currTime
-            if record:
-                # st.checkbox("Recording", value=True)
-                out.write(frame)
             # Dashboard
             kpi1_text.write(f"<h1 style='text-align: center; color: red;'>{int(fps)}</h1>", unsafe_allow_html=True)
-            kpi2_text.write(f"<h1 style='text-align: center; color: red;'>{face_count}</h1>", unsafe_allow_html=True)
-            kpi3_text.write(f"<h1 style='text-align: center; color: red;'>{width}</h1>", unsafe_allow_html=True)
+            kpi2_text.write(f"<h1 style='text-align: center; color: red;'>{label}</h1>", unsafe_allow_html=True)
 
-            frame = cv2.resize(frame, (0, 0), fx=0.8, fy=0.8)
-            frame = image_resize(image=frame, width=640)
-            stframe.image(frame, channels='BGR', use_column_width=True)
-
-    st.text('Video Processed')
-
-    output_video = open('output1.mp4', 'rb')
-    out_bytes = output_video.read()
-    st.video(out_bytes)
-
-    vid.release()
-    out.release()
+cap.release()
+out.release()
 
 elif app_mode =='Signal check':
 
